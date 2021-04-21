50d3f12a0a8e08118e62bbb74314f2875e1874a6
Tormod <tormod.flesjo@gmail.com>
2021-04-21 14:12:09 +0200

    remove debug package

--- server/app.js
+++ server/app.js
@@ -8,12 +8,6 @@ const express = require('express')
 const logger = require('morgan')
 const nocache = require('nocache')
 
-
-const app_name = require('./package.json').name
-const debug = require('debug')(
-  `${app_name}:${path.basename(__filename).split('.')[0]}`
-)
-
 const app = express()
 
 app.use(nocache())
